f6dfef60cc28c5d144fcde55217ecdde2bee8460
Vasyl Babala <babala.vasyl@gmail.com>
2020-05-16 00:38:26 +0300

    basic backend folders, logic

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportController } from './report/report.controller';
+import { ProjectController } from './project/project.controller';
+import { UserController } from './user/user.controller';
+import { ProjectService } from './project/project.service';
+import { ReportService } from './report/report.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ReportController, ProjectController, UserController],
+  providers: [AppService, ProjectService, ReportService, UserService],
 })
 export class AppModule {}
